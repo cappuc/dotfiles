51417e1c87693608d1d0481b391645f6e41613c9
Fabio Capucci <fabio.cappuc@gmail.com>
2017-12-03 11:02:32 +0100

    Update

--- path.zsh
+++ path.zsh
@@ -12,6 +12,7 @@ export PATH="node_modules/.bin:vendor/bin:$PATH"
 
 # Make sure PHP 7.1 is loaded
 export PATH="$(brew --prefix homebrew/php/php71)/bin:$PATH"
+export XDEBUG_CONFIG="ideKey=VSCODE"
 
 # Make sure coreutils are loaded before system commands
 # I've disabled this for now because I only use "ls" which is
