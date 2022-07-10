194ccaee8abc36693c969c90ed2edb07b0676051
Fabio Capucci <fabio.cappuc@gmail.com>
2022-07-10 13:45:33 +0200

    update

--- aliases.zsh
+++ aliases.zsh
@@ -3,7 +3,6 @@ alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
 alias reloadcli="source $HOME/.zshrc"
 alias reloaddns="dscacheutil -flushcache && sudo killall -HUP mDNSResponder"
 alias ll="$(brew --prefix coreutils)/libexec/gnubin/ls -ahlF --color --group-directories-first"
-alias phpstorm='open -a /Applications/PhpStorm.app "`pwd`"'
 
 # Directories
 alias dotfiles="cd $DOTFILES"
@@ -17,32 +16,14 @@ alias yd="yarn run dev"
 alias yp="yarn run prod"
 
 # Composer
-#unalias cgr
-
-function devl() {
-  dev && cd laravel/$1
-}
-
-function devp() {
-  dev && cd packages/$1
-}
-
-function devw() {
-  dev && cd wordpress/$1
-}
-
-function devn() {
-  dev && cd node/$1
-}
+unalias cgr
 
 function devgo() {
   cd $GOPATH/src/$1
 }
 
-alias nvidia_update='bash <(curl -s https://raw.githubusercontent.com/Benjamin-Dobell/nvidia-update/master/nvidia-update.sh)'
-
 function composer-link() {  
-    composer config repositories.local '{"type": "path", "url": "'$1'"}' --file composer.json
+    composer config repositories.${2:-local} path "$1"
 }
 
 alias dyn="dynamodb-local -port 1337"
